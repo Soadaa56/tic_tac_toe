bd632da32d592f21fc12c1c7892c859e05457abd
soadaa <Soadhlh@gmail.com>
2022-08-10 19:56:31 -0400

    methods start_game and show_game_board
    This also added public and private sections and changed my lines and game_board variables to class variables.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,9 +3,9 @@ require 'pry-byebug'
 
 class TicTacToe
 
-  lines = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+  @@lines = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 
-  game_board = '
+  @@game_board = '
      |     |     
  1   |  2  |  3  
 _____|_____|_____
@@ -16,18 +16,29 @@ _____|_____|_____
  7   |  8  |  9  
      |     |     
   '
-  puts "Input player 1 name:"
-  player1 = gets.chomp
-  puts "Input player 2 name:"
-  player2 = gets.chomp
-  puts "\n"
 
-  puts "#{player1} input the numbered square you would wish to place an x"
+  private 
 
+  def get_player_names
+    puts "Input player 1 name:"
+    player1 = gets.chomp
+    puts "Input player 2 name:"
+    player2 = gets.chomp
+  end
+  
+
+  # puts "#{player1} input the numbered square you would wish to place an x"
+  public
+
+  def start_game
+    get_player_names
+    show_game_board
+  end
+
+  def show_game_board
+    puts @@game_board
+  end
 end
 
-class Player
-end
-
-class HumanPlayer < Player
-end
+game = TicTacToe.new
+game.start_game
